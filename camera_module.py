b205b102dd2fac12b43591b8a312e0fe7c79e1aa
wileik15 <william.eikrem@hotmail.no>
2021-03-23 14:29:30 +0100

    Initial commit for blendCamera class.

---
+++ camera_module.py
@@ -0,0 +1,36 @@
+import bpy
+
+
+class blendCamera:
+
+    structured_light = True
+    sensor_width = 36
+
+
+    @classmethod
+    def import_camera(cls):
+        """
+        Method for importing camera object in Blender scene
+
+        :param structured_light: Determine if camera object is structured_light or structured light camera
+        :type structured_light: Bool
+        """
+
+        bpy.ops.object.camera_add()
+        cam = bpy.context.active_object
+        cam.name = camera
+        bpy.data.cameras[cam.name].sensor_width = blendCamera.sensor_width
+
+        if blendCamera.structured_light:
+            print("Structured light is activated")
+
+        else:
+            print("Simple camera is activated")
+
+
+if __name__ = '__main__':
+
+    blendCamera.structured_light = True
+
+
+    blendCamera.import_camera(False)
